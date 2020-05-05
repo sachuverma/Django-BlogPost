e6be7b7305b0c7c794e4ddf82a800db0199c3156
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2020-05-05 16:00:29 +0530

    static setting

--- django_project/settings.py
+++ django_project/settings.py
@@ -126,16 +126,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
-
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
