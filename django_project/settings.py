344b0a12b43d2006b23b42ab2b0fcf4ee69a9b2a
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2020-05-17 17:05:52 +0530

    add allowed host

--- django_project/settings.py
+++ django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'exhlfdat&vfum(-34*c2uroi(($ww(yo$9pv98=e6p^gl(-eoj'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG_APP');
 
-ALLOWED_HOSTS = ['sachu-django-blog.herokuapp.com/', 'localhost']
+ALLOWED_HOSTS = ['sachu-django-blog.herokuapp.com', 'localhost']
 
 
 # Application definition
