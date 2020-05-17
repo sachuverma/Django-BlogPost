e8c44f299725d949bfc14b950d7aded88699cec7
sachin verma <43083013+sachinverma53121@users.noreply.github.com>
2020-05-17 23:30:47 +0530

    django-heroku disable

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -146,4 +146,4 @@ EMAIL_HOST_PASSWORD = os.environ.get('MY_TEST_EMAIL_PASSWORD')
 
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
